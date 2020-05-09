bb5552c46cf569955e35ef14e377b1c941b040f2
Ganga Adhikari <54366082+gangaadhikari@users.noreply.github.com>
2020-05-08 21:58:53 -0400

    Update stocks.py
    Trained the data SVM and LR

--- stocks.py
+++ stocks.py
@@ -1,5 +1,5 @@
 import quandl
-import numpy
+import numpy as np
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -17,7 +17,43 @@ print(df.head())
 #Number of days to forecast
 forecastDays = 1
 
-#Create another column (the target) shifter 'n' units up
-df['Prediction'] = df[['Adj.Close']].shift(-1)
+#Create another column (the target) shifter 'forecastDays' units up
+df['Prediction'] = df[['Adj. Close']].shift(-forecastDays)
 #print new data set
-print(df.head())
+print(df.tail())
+
+#Create the independent seta set (X)
+#Conver the dataframe to a numpy array
+X = np.array(df.drop(['Prediction'],1))
+#Remove the last 'forecasrDays'
+X = X[:-forecastDays]
+print(X)
+
+#Create the dependent data set (y)
+#Convert the dataframe to a numpy array (All of the values including the NaN)
+y = np.array(df['Prediction'])
+#Get all the target values (y) except the forecastDays
+y = y[:-forecastDays]
+print(y)
+
+#Split the data into 80% training and 20% testing
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+#Create and train the Support Vector Machine (Regressor)
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+svr_rbf.fit(x_train, y_train)
+
+#Testing Model: Score return the coefficient of determination R^2 of the prediction
+#The best posssible score = 1
+svm_confidence = svr_rbf.score(x_test, y_test)
+print("svm confidence: ", svm_confidence)
+
+#Create and train the Linear Regression Model 
+lr = LinearRegression()
+#Train the model
+lr.fit(x_train, y_train)
+
+#Testing Model: Score return the coefficient of determination R^2 of the prediction
+#The best posssible score = 1
+lr_confidence = lr.score(x_test, y_test)
+print("lr confidence: ", lr_confidence)
